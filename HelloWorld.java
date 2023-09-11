92c1f35e78740b06db9fb6c488b3f07204003121
H S Chaudhary <85777912+hschaudhary06@users.noreply.github.com>
2023-09-11 11:53:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String args[]){
         System.out.println("********** Hello world & changes on GitHub **********");
+        System.out.println("***** trying webhook *****");
     }
 }
